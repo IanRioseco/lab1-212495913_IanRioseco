aca3ab646619273dae65a680be44dd936d8cb653
ian rioseco <ian.rioseco@usach.cl>
2024-11-01 19:27:33 -0300

    version final final

--- codigos/SCRIPT1_212495913_IanRiosecoCastro.rkt
+++ codigos/SCRIPT1_212495913_IanRiosecoCastro.rkt
@@ -4,6 +4,9 @@
 (require "TDAboard.rkt")
 (require "TDAgame.rkt")
 
+(display "----------------------------------PRUEBA 1------------------------------------------------")
+(newline)
+(newline)
 
 ; 1. Creación de jugadores (21 fichas cada uno para un juego largo)
 (define p1 (player 1 "Juan" "red" 0 0 0 21))
@@ -101,10 +104,174 @@
 (display "Historial de movimientos: ")
 (newline)
 (game-history ended-game)
+(newline)
 
 ; 11. Mostrar estado final del tablero
 (display "Estado final del tablero: ")
 (newline)
 (game-get-board ended-game)
-
+(newline)
 (display ended-game)
+(newline)
+(newline)
+
+(display "----------------------------------PRUEBA 2------------------------------------------------")
+(newline)
+(newline)
+
+; 1. Creación de jugadores
+(define player1 (player 1 "Luis" "red" 0 0 0 21))
+(define player2 (player 2 "Ana" "yellow" 0 0 0 21))
+
+; 2. Creación de piezas
+(define redpiece (piece "red"))
+(define yellowpiece (piece "yellow"))
+
+; 3. Creación del tablero inicial
+(define board-empty board)
+
+; 4. Creación de un nuevo juego
+(define game0 (game player1 player2 board-empty 1))
+
+; 5. Realizando movimientos para victoria horizontal de "Luis"
+(define game1 (game-player-set-move game0 player1 0))  
+(define game2 (game-player-set-move game1 player2 1))  
+(define game3 (game-player-set-move game2 player1 0))  
+(define game4 (game-player-set-move game3 player2 1))  
+(define game5 (game-player-set-move game4 player1 2))  
+(define game6 (game-player-set-move game5 player2 3))  
+(define game7 (game-player-set-move game6 player1 4))  
+(define game8 (game-player-set-move game7 player2 0))  
+(define game9 (game-player-set-move game8 player1 2))  
+(define game10 (game-player-set-move game9 player2 3)) 
+(define game11 (game-player-set-move game10 player1 4))
+(define game12 (game-player-set-move game11 player2 3))
+(define game13 (game-player-set-move game12 player1 4))
+(define game14 (game-player-set-move game13 player2 2))
+(define game15 (game-player-set-move game14 player1 6))
+(define game16 (game-player-set-move game15 player2 1))
+
+; 6. Verificaciones
+(display "¿Se puede jugar en el tablero? ")
+(board-can-play? (game-get-board game16))
+
+(display "Jugador actual: ")
+(game-get-current-player game16)
+
+(display "Verificación de victoria vertical: ")
+(board-check-vertical-win (game-get-board game16))
+
+(display "Verificación de victoria horizontal: ")
+(board-check-horizontal-win (game-get-board game16))
+
+(display "Verificación de victoria diagonal: ")
+(board-check-diagonal-win (game-get-board game16))
+
+(display "Verificación de ganador: ")
+(board-who-is-winner (game-get-board game16))
+
+(display "¿Es empate? ")
+(game-is-draw? game16)
+
+; 7. Finalizar el juego y actualizar estadísticas
+(define end-game (game-set-end game16))
+
+
+; 8. Mostrar historial de movimientos
+(display "Historial de movimientos: ")
+(newline)
+(game-history end-game)
+(newline)
+
+; 9. Estado final del tablero
+(display "Estado final del tablero: ")
+(newline)
+(game-get-board end-game)
+(newline)
+(display end-game)
+(newline)
+
+
+
+
+
+(display "----------------------------------PRUEBA 3------------------------------------------------")
+(newline)
+
+; 1. Creación de jugadores
+(define jugador1 (player 1 "Carlos" "red" 0 0 0 21))
+(define jugador2 (player 2 "Lucía" "yellow" 0 0 0 21))
+
+; 2. Creación de piezas
+(define pieza-roja (piece "red"))
+(define pieza-amarilla (piece "yellow"))
+
+; 3. Creación del tablero inicial
+(define tablero-vacio board)
+
+; 4. Creación de un nuevo juego
+(define juego0 (game jugador1 jugador2 tablero-vacio 1))
+
+; 5. Realizando movimientos para victoria diagonal de "Lucía"
+(define juego1 (game-player-set-move juego0 jugador1 0))     ; Carlos coloca en columna 0
+(define juego2 (game-player-set-move juego1 jugador2 1))     ; Lucía coloca en columna 1
+(define juego3 (game-player-set-move juego2 jugador1 1))     ; Carlos coloca en columna 1
+(define juego4 (game-player-set-move juego3 jugador2 2))     ; Lucía coloca en columna 2
+(define juego5 (game-player-set-move juego4 jugador1 2))     ; Carlos coloca en columna 2
+(define juego6 (game-player-set-move juego5 jugador2 3))     ; Lucía coloca en columna 3
+(define juego7 (game-player-set-move juego6 jugador1 2))     ; Carlos coloca en columna 2
+(define juego8 (game-player-set-move juego7 jugador2 3))     ; Lucía coloca en columna 3
+(define juego9 (game-player-set-move juego8 jugador1 3))     ; Carlos coloca en columna 3
+(define juego10 (game-player-set-move juego9 jugador2 1))    ; Lucía coloca en columna 3
+(define juego11 (game-player-set-move juego10 jugador1 0))   ; Carlos coloca en columna 0
+(define juego12 (game-player-set-move juego11 jugador2 0))   ; Lucía coloca en columna 0
+(define juego13 (game-player-set-move juego12 jugador1 4))   ; Carlos coloca en columna 4
+(define juego14 (game-player-set-move juego13 jugador2 1))   ; Lucía coloca en columna 1
+(define juego15 (game-player-set-move juego14 jugador1 4))   ; Carlos coloca en columna 4
+(define juego16 (game-player-set-move juego15 jugador2 1))   ; Lucía coloca en columna 1
+(define juego17 (game-player-set-move juego16 jugador1 4))   ; Carlos coloca en columna 4
+(define juego18 (game-player-set-move juego17 jugador2 2))   ; Lucía coloca en columna 2
+(define juego19 (game-player-set-move juego18 jugador1 6))   ; Carlos coloca en columna 4
+(define juego20 (game-player-set-move juego19 jugador2 2))   ; Lucía coloca en columna 2
+(define juego21 (game-player-set-move juego20 jugador1 1))   ; Carlos coloca en columna 1
+(define juego22 (game-player-set-move juego21 jugador2 5))   ; Lucía coloca en columna 3 (Victoria diagonal)
+(define juego23 (game-player-set-move juego22 jugador1 3))   ; Carlos coloca en columna 3 (Victoria diagonal)
+
+; 6. Verificaciones
+(display "¿Se puede jugar en el tablero? ")
+(board-can-play? (game-get-board juego23))
+
+(display "Jugador actual: ")
+(game-get-current-player juego23)
+
+(display "Verificación de victoria vertical: ")
+(board-check-vertical-win (game-get-board juego23))
+
+(display "Verificación de victoria horizontal: ")
+(board-check-horizontal-win (game-get-board juego23))
+
+(display "Verificación de victoria diagonal: ")
+(board-check-diagonal-win (game-get-board juego23))
+
+(display "Verificación de ganador: ")
+(board-who-is-winner (game-get-board juego23))
+
+(display "¿Es empate? ")
+(game-is-draw? g23)
+
+; 7. Finalizar el juego y actualizar estadísticas
+(define juego-terminado (game-set-end juego23))
+
+; 8. Mostrar historial de movimientos
+(display "Historial de movimientos: ")
+(newline)
+(game-history juego-terminado)
+(newline)
+
+; 9. Estado final del tablero
+(display "Estado final del tablero: ")
+(newline)
+(game-get-board juego-terminado)
+(newline)
+(display juego-terminado)
+(newline)
--- codigos/SCRIPT2_212495913_IanRiosecoCastro.rkt
+++ codigos/SCRIPT2_212495913_IanRiosecoCastro.rkt
@@ -4,7 +4,8 @@
 (require "TDAboard.rkt")
 (require "TDAgame.rkt")
 
-
+(display "----------------------------------PRUEBA 1------------------------------------------------")
+(newline)
 ; 1. Creación de jugadores
 (define p1 (player 1 "Thomas" "red" 0 0 0 10))
 (define p2 (player 2 "Isi" "yellow" 0 0 0 10))
@@ -13,8 +14,12 @@
 (define red-piece (piece "red"))
 (define yellow-piece (piece "yellow"))
 
-; 3. Creación del tablero inicial
+; 3. Creación del tablero inicial y verificación de jugabilidad
 (define empty-board board)
+(display "¿Puede jugar en el tablero vacío? ")
+(display (board-can-play? empty-board))
+(newline)
+(newline)
 
 ; 4. Creación de un nuevo juego
 (define g0 (game p1 p2 empty-board 1))
@@ -95,3 +100,176 @@
 (display "Estado final del tablero: ")
 (newline)
 (game-get-board ended-game)
+(newline)
+
+(display "----------------------------------PRUEBA 2------------------------------------------------")
+(newline)
+
+; 1. Creación de jugadores
+(define player1 (player 1 "jose" "red" 0 0 0 21))
+(define player2 (player 2 "benja" "yellow" 0 0 0 21))
+
+; 2. Creación de piezas
+(define piece-red (piece "red"))
+(define piece-yellow (piece "yellow"))
+
+; 3. Creación del tablero inicial y verificación de jugabilidad
+(define board-empty board)
+(display "¿Puede jugar en el tablero vacío? ")
+(display (board-can-play? board-empty))
+(newline)
+(newline)
+
+; 4. Creación de un nuevo juego
+(define game0 (game player1 player2 board-empty 1))
+
+; 5. Simulación de movimientos para una victoria vertical del jugador 1 en la columna 3
+(define game1 (game-player-set-move game0 player1 3))
+(define game2 (game-player-set-move game1 player2 2))
+(define game3 (game-player-set-move game2 player1 3))
+(define game4 (game-player-set-move game3 player2 2))
+(define game5 (game-player-set-move game4 player1 3))
+(define game6 (game-player-set-move game5 player2 2))
+(define game7 (game-player-set-move game6 player1 3)) ; Victoria vertical del jugador 1
+
+; 6. Verificación de victoria
+(display "Verificación de victoria vertical: ")
+(display (board-check-vertical-win (game-get-board game7)))
+(newline)
+(newline)
+
+(display "Verificación de victoria horizontal: ")
+(display (board-check-horizontal-win (game-get-board game7)))
+(newline)
+(newline)
+
+(display "Verificación de victoria diagonal: ")
+(display (board-check-diagonal-win (game-get-board game7)))
+(newline)
+(newline)
+
+(display "¿Quién es el ganador? ")
+(display (board-who-is-winner (game-get-board game7)))
+(newline)
+(newline)
+
+; 7. Intento de agregar pieza cuando ya está ocupado
+(display "Intento de agregar pieza en columna 3 nuevamente: ")
+(display (board-set-play-piece (game-get-board game7) 3 red-piece))
+(newline)
+(newline)
+
+; 8. Verificación de empate
+(display "Verificación de empate: ")
+(display (game-is-draw? game7))
+(newline)
+(newline)
+
+; 9. Estado final del tablero
+(display "Estado final del tablero: ")
+(newline)
+(display (game-get-board game7))
+(newline)
+(newline)
+
+; 10. Historial de movimientos
+(display "Historial de movimientos: ")
+(newline)
+(display (game-history game7))
+(newline)
+(newline)
+
+
+; 11. Finalizar el juego y mostrar estado final del tablero
+(display "Finalización del juego y estado del tablero: ")
+(newline)
+(define end-game (game-set-end game7))
+(display end-game)
+(newline)
+(newline)
+(display (game-get-board end-game))
+(newline)
+(newline)
+
+
+(display "----------------------------------PRUEBA 3------------------------------------------------")
+(newline)
+(newline)
+
+; 1. Creación de jugadores
+(define jugador1 (player 1 "juan" "yellow" 0 0 0 10))
+(define jugador2 (player 2 "vicente" "red" 0 0 0 10))
+
+; 2. Creación de piezas
+(define pieza-roja (piece "red"))
+(define pieza-amarilla (piece "yellow"))
+
+; 3. Creación del tablero inicial y verificación de jugabilidad
+(define tablero-vacio board)
+(display "¿Puede jugar en el tablero vacío? ")
+(display (board-can-play? tablero-vacio))
+(newline)
+(newline)
+
+; 4. Creación de un nuevo juego
+(define juego0 (game jugador1 jugador2 tablero-vacio 1))
+
+; 5. Simulación de movimientos que llenan el tablero sin victoria (empate)
+(define juego1 (game-player-set-move juego0 jugador1 0))
+(define juego2 (game-player-set-move juego1 jugador2 1))
+(define juego3 (game-player-set-move juego2 jugador1 2))
+(define juego4 (game-player-set-move juego3 jugador2 3))
+(define juego5 (game-player-set-move juego4 jugador1 4))
+(define juego6 (game-player-set-move juego5 jugador2 5))
+(define juego7 (game-player-set-move juego6 jugador1 6))
+; Continúa llenando el tablero alternando entre columnas y jugadores...
+
+; 6. Verificación de victoria y empate
+(display "Verificación de victoria vertical: ")
+(display (board-check-vertical-win (game-get-board juego7)))
+(newline)
+(newline)
+
+(display "Verificación de victoria horizontal: ")
+(display (board-check-horizontal-win (game-get-board juego7)))
+(newline)
+(newline)
+
+(display "Verificación de victoria diagonal: ")
+(display (board-check-diagonal-win (game-get-board juego7)))
+(newline)
+(newline)
+
+(display "¿Quién es el ganador? ")
+(display (board-who-is-winner (game-get-board juego7)))
+(newline)
+(newline)
+
+(display "Verificación de empate: ")
+(display (game-is-draw? juego7))
+(newline)
+(newline)
+
+; 7. Estado final del tablero
+(display "Estado final del tablero: ")
+(newline)
+(display (game-get-board juego7))
+(newline)
+(newline)
+
+; 8. Historial de movimientos
+(display "Historial de movimientos: ")
+(newline)
+(display (game-history juego7))
+(newline)
+(newline)
+
+
+; 9. Finalizar el juego y mostrar estado final del tablero
+(display "Finalización del juego y estado del tablero: ")
+(newline)
+(define juego-terminado (game-set-end juego7))
+(display juego-terminado)
+(newline)
+(newline)
+(display (game-get-board juego-terminado))
